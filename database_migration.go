993b3b57dba1bba9d512208eab48e7329797c3a2
Andi Ashari <mq.aashari@gmail.com>
2018-11-09 19:47:19 +0700

    Update migrate data

--- database_migration.go
+++ database_migration.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
+	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 	"log"
+	"net/url"
+	"os"
+	"ralali.com/models"
 )
 
 func init() {
@@ -14,5 +19,39 @@ func init() {
 }
 
 func main() {
+
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASS")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	defaultTimezone := os.Getenv("SERVER_TIMEZONE")
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=1&loc=%s",
+		dbUser,
+		dbPass,
+		dbHost,
+		dbPort,
+		dbName,
+		url.QueryEscape(defaultTimezone),
+	)
+
+	fmt.Println(connection)
+
+	db, err := gorm.Open("mysql", connection)
+	if nil != err {
+		log.Fatal(err)
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
+	// migrate user
+	db.AutoMigrate(&models.User{})
+
+	// migrate order
+	db.AutoMigrate(&models.Order{})
+	db.Model(&models.Order{}).AddForeignKey("user_id", "users(id)", "RESTRICT", "RESTRICT")
 
 }
