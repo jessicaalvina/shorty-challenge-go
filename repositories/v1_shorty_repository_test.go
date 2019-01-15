378e0aef4f89d91edfd06b32dc5e8d0beb8087a6
Jessica Alvina <skypearl@macbooks-MacBook-Pro-2.local>
2019-01-15 11:15:35 +0700

    add repository test case

--- repositories/v1_shorty_repository_test.go
+++ repositories/v1_shorty_repository_test.go
@@ -1,11 +1,111 @@
 package repositories
 
-import "testing"
+import (
+	"os"
+	"fmt"
+	"net/url"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"testing"
+	"github.com/joho/godotenv"
+	"../objects"
+	// "github.com/jinzhu/gorm"
+) 
 
-func TestV1ShortyRepository_GetByShortcode(t *testing.T) {
+var db *gorm.DB
+
+func init() {
+
+	// load env
+	godotenv.Load("../.env")
+
+	// db init
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASS")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	defaultTimezone := os.Getenv("SERVER_TIMEZONE")
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=1&loc=%s",
+		dbUser,
+		dbPass,
+		dbHost,
+		dbPort,
+		dbName,
+		url.QueryEscape(defaultTimezone),
+	)
+
+	db, err := gorm.Open("mysql", connection)
+	if nil != err {
+		panic(err)
+	}
+
+	db.LogMode(true)
+
+	defer db.Close()
 
 }
 
-func TestV1ShortyRepository_GetByShortcodeStats(t *testing.T) {
+func TestV1ShortyRepository_GetByShortcodeFailed(t *testing.T) {
 
+	repository := V1ShortyRepositoryHandler(db.Begin())
+	_, err := repository.GetByShortcode("example")
+	
+	if err == nil {
+		t.Errorf("Failed. Example shortcode cant be found in the system")
+	}
+}
+
+func TestV1ShortyRepository_GetByShortcodeSuccess(t *testing.T) {
+
+	repository := V1ShortyRepositoryHandler(db.Begin())
+	_, err := repository.GetByShortcode("google")
+	
+	if err != nil {
+		t.Errorf("Success. Google shortcode found in the system, redirecting...")
+	}
+}
+
+func TestV1ShortyRepository_UpdateRedirectCountFailed(t *testing.T) {
+	repository := V1ShortyRepositoryHandler(db.Begin())
+	postObject := objects.V1ShortyObjectRequest{Shortcode: "bingobingo"}
+
+	errBool := repository.UpdateRedirectCount("apple", postObject)
+	
+	if errBool == false {
+		t.Errorf("Failed. Can't update redirect count for apple shortcode")
+	}
+}
+
+func TestV1ShortyRepository_UpdateRedirectCountSuccess(t *testing.T) {
+	repository := V1ShortyRepositoryHandler(db.Begin())
+	postObject := objects.V1ShortyObjectRequest{Shortcode: "google"}
+
+	errBool := repository.UpdateRedirectCount("google",postObject)
+	
+	if errBool != false {
+		t.Errorf("Success. Add 1 for redirect count for google shortcode")
+	}
+}
+
+func TestV1ShortyRepository_GetByShortcodeStatsFailed(t *testing.T) {
+
+	repository := V1ShortyRepositoryHandler(db.Begin())
+	_, err := repository.GetByShortcodeStats("example")
+	
+	if err == nil {
+		t.Errorf("Failed. Example shortcode cant be found in the system")
+	}
+}
+
+func TestV1ShortyRepository_GetByShortcodeStatsSuccess(t *testing.T) {
+
+	repository := V1ShortyRepositoryHandler(db.Begin())
+	_, err := repository.GetByShortcodeStats("google")
+	
+	if err != nil {
+		t.Errorf("Success. Show google shortcode stats")
+	}
 }
--- repositories/v2_shorty_repository_test.go
+++ repositories/v2_shorty_repository_test.go
@@ -1,11 +1,94 @@
 package repositories
 
-import "testing"
+import (
+	"fmt"
+	"net/url"
+	"os"
+	"testing"
 
-func TestV1ShortyRepository_GetByShortcode(t *testing.T) {
+	"../objects"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jinzhu/gorm"
+	"github.com/joho/godotenv"
+	// "github.com/jinzhu/gorm"
+)
+
+// var db *gorm.DB
+
+func init() {
+
+	// load env
+	godotenv.Load("../.env")
+
+	// db init
+	dbHost := os.Getenv("DATABASE_HOST")
+	dbPort := os.Getenv("DATABASE_PORT")
+	dbUser := os.Getenv("DATABASE_USER")
+	dbPass := os.Getenv("DATABASE_PASS")
+	dbName := os.Getenv("DATABASE_NAME")
+
+	defaultTimezone := os.Getenv("SERVER_TIMEZONE")
+
+	connection := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=2&loc=%s",
+		dbUser,
+		dbPass,
+		dbHost,
+		dbPort,
+		dbName,
+		url.QueryEscape(defaultTimezone),
+	)
+
+	db, err := gorm.Open("mysql", connection)
+	if nil != err {
+		panic(err)
+	}
+
+	db.LogMode(true)
+
+	defer db.Close()
 
 }
 
-func TestV1ShortyRepository_GetByShortcodeStats(t *testing.T) {
+func TestV2ShortyRepository_PostByShortenRegexFailed(t *testing.T) {
+	repository := V2ShortyRepositoryHandler(db.Begin())
+	postObject := objects.V2ShortyObjectRequest{Shortcode: "bingobingo", Url: "http://www.bingo.com"}
 
+	_, err := repository.PostByShorten(postObject)
+
+	if err == nil {
+		t.Errorf("Failed. Shortcode is not meet following regexep ^[0-9a-zA-Z_]{4,}$")
+	}
+}
+
+func TestV2ShortyRepository_PostByShortenUrlFailed(t *testing.T) {
+	repository := V2ShortyRepositoryHandler(db.Begin())
+	postObject := objects.V2ShortyObjectRequest{Shortcode: "bottle", Url: "w.bottlecom"}
+
+	_, err := repository.PostByShorten(postObject)
+
+	if err == nil {
+		t.Errorf("Failed. Url is not present")
+	}
+}
+
+func TestV2ShortyRepository_PostByShortenDuplicateFailed(t *testing.T) {
+	repository := V2ShortyRepositoryHandler(db.Begin())
+	postObject := objects.V2ShortyObjectRequest{Shortcode: "google", Url: "http://www.google.com"}
+
+	_, err := repository.PostByShorten(postObject)
+
+	if err == nil {
+		t.Errorf("Failed. google shortcode has been created before")
+	}
+}
+
+func TestV2ShortyRepository_PostByShortenSuccess(t *testing.T) {
+	repository := V2ShortyRepositoryHandler(db.Begin())
+	postObject := objects.V2ShortyObjectRequest{Shortcode: "yahhoo", Url: "http://www.yahoo.com"}
+
+	_, err := repository.PostByShorten(postObject)
+
+	if err != nil {
+		t.Errorf("Success. yahhoo shortcode created with http://www.yahoo.com url")
+	}
 }
